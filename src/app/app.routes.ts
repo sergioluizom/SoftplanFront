78526b65bf8397f88184175f76125eefb660890a
Sergio Luiz de Oliveira Machado <sergioluizom@gmail.com>
2021-03-21 17:54:43 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './navegacao/home/home.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
+
+export const rootRouterConfig: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'contato', component: ContatoComponent },    
+    { path: 'produtos', component: ListaProdutoComponent },
+    { path: 'produto-detalhe/:id', component: ListaProdutoComponent }
+];
